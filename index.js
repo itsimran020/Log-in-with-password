ce9712f534918ddb294062a8773363836b569195
imran ansari <140491773+itsimran020@users.noreply.github.com>
2024-03-23 16:21:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let viewPass = document.querySelector("#img1");
+let pass = document.querySelector("#password");
+let logIn = document.querySelector("button");
+let container = document.querySelector(".input-box");
+
+viewPass.addEventListener("click", () => {
+  if (pass.type == "password") {
+    pass.type = "text";
+    viewPass.src = "eye-open.png";
+  } else {
+    pass.type = "password";
+    viewPass.src = "eye-close.png";
+  }
+});
+
+pass.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (pass.value == "20012021") {
+      window.location.href = "https://www.youtube.com/shorts/bNQd1B88BxU";
+    } else {
+      pass.type = "text";
+      container.style.border = "2px solid red";
+      pass.style.color = "red";
+      pass.value = "";
+      pass.placeholder = "Password incorect, re-enter please";
+      pass.type = "password";
+    }
+  }
+});
+
+logIn.addEventListener("click", () => {
+  if (pass.value == "20012021") {
+    window.location.href = "index2.html";
+  } else {
+    pass.type = "text";
+    container.style.border = "1px solid red";
+    pass.style.color = "red";
+    pass.value = "";
+    pass.placeholder = "Password incorect, re-enter please";
+    pass.type = "password";
+  }
+});
